6af04b9aa8817a94ab415b328592feaa03833ce6
David Chemouil <david.chemouil+electrum@onera.fr>
2015-10-31 23:28:11 +0100

    release

--- util/bump-version.sh
+++ util/bump-version.sh
@@ -6,7 +6,7 @@ set -e
 # for a git flow release of the form release/0.x.x
 VERSION=`git rev-parse --abbrev-ref HEAD | cut -f 2- -d '/'`
 
-sed -i "s/Version:.*\$/Version: $VERSION/" _oasis
+sed -i "s/^Version:.*\$/Version: $VERSION/" _oasis
 
 sed -i "s/^version:.*\$/version: \"$VERSION\"/" opam
 
